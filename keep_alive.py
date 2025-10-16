680654c9e57ed2d7734f9587d3ae9490516e1f6c
thenight2k6 <121368973+thenight2k6@users.noreply.github.com>
2025-10-16 20:46:45 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from threading import Thread
+
 app = Flask('')
+
 @app.route('/')
-def home():
-    return "OK", 200
+def main():
+    return '<meta http-equiv="refresh" content="0; URL=https://phantom.fr.to/support"/>'
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
